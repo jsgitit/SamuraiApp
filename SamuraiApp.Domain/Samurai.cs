91de2ffc0139921808396ca07cc4178d2992307c
Jon Smart <jsmartware@gmail.com>
2020-07-03 19:26:12 -0700

    small change

--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -12,7 +12,7 @@ namespace SamuraiApp.Domain
         public string Name { get; set; }
         public List<Quote> Quotes { get; set; }
         public Clan Clan { get; set; }
-        
+         
     }
 }
 
