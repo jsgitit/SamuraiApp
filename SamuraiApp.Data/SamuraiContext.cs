a289a954272d06e73bf615289447222e7c56c92d
Jon Smart <jsmartware@gmail.com>
2020-07-05 19:55:35 -0700

    I completed CH03 and started CH04, made changes to add new tables to the database.  Ready to Migrate the table changes.

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -1,19 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Domain;
-using System;
 
 namespace SamuraiApp.Data
 {
-    public class SamuraiContext: DbContext
+    public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
-
+        public DbSet<Battle> Battles { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionstring = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData";
             optionsBuilder.UseSqlServer(connectionstring);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
         }
     }
 }
--- SamuraiApp.Domain/Samurai.cs
+++ SamuraiApp.Domain/Samurai.cs
@@ -7,12 +7,18 @@ namespace SamuraiApp.Domain
         public Samurai()
         {
             Quotes = new List<Quote>();
+            SamuraiBattles = new List<SamuraiBattle>();
+
         }
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Quote> Quotes { get; set; }
         public Clan Clan { get; set; }
-         
+        public List<SamuraiBattle> SamuraiBattles { get; set; }
+        // Horse is always optional in this 1:1 relationship. 
+        // Use BL to control whether or not a Samurai has a Horse.
+        public Horse Horse { get; set; } 
+
     }
 }
 
